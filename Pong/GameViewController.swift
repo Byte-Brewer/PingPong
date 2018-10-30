1637541408f3df78c428e7662b240b231e1935b4
pivowar <2008Nazar@gmail.com>
2018-10-30 14:54:08 +0200

    first commit

--- Pong/GameViewController.swift
+++ Pong/GameViewController.swift
@@ -9,42 +9,98 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import ReplayKit
 
 class GameViewController: UIViewController {
-
+    
+    let cameraController = CameraController()
+    let recorder = RPScreenRecorder.shared()
+    
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var gameView: SKView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
+        if let view = self.gameView {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+                scene.backgroundColor = UIColor.clear
+        
                 // Present the scene
                 view.presentScene(scene)
             }
             
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        
+        // turn on Microphone
+        recorder.isMicrophoneEnabled = true
     }
 
+    
+    @IBAction func openCamera(_ sender: UIButton) {
+        DispatchQueue.global().async {
+            self.configureCameraController()
+        }
+    }
+    
+    @IBAction func rec(_ sender: UIButton) {
+        if !recorder.isRecording {
+            recorder.startRecording { (error) in
+                guard error != nil else { return }
+            }
+        }
+    }
+    
+    @IBAction func stop(_ sender: UIButton) {
+        if recorder.isRecording {
+            recorder.stopRecording { (previewVC, error) in
+                if let previewVC = previewVC {
+                    previewVC.previewControllerDelegate = self
+                    self.present(previewVC, animated: true, completion: nil)
+                }
+                
+                if let error = error  {
+                    print("Error", error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    private func configureCameraController() {
+        cameraController.prepare {(error) in
+            if let error = error {
+                print(error)
+            }
+            try? self.cameraController.displayPreview(on: self.cameraView)
+        }
+    }
+}
+
+// MARK:- RPPreviewViewControllerDelegate
+extension GameViewController: RPPreviewViewControllerDelegate {
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func previewController(_ previewController: RPPreviewViewController, didFinishWithActivityTypes activityTypes: Set<String>) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
